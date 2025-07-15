152685376214813b509b0b2dd5b69aa84162a020
muhammad230 <ali.khansmr3@gmail.com>
2025-07-15 12:57:39 +0500

    choise Favcolor

--- src/component/color.js
+++ src/component/color.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
 
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button onClick={() => setColor("blue")}>Blue</button>
+      <button onClick={() => setColor("red")}>Red</button>
+      <button onClick={() => setColor("pink")}>Pink</button>
+      <button onClick={() => setColor("green")}>Green</button>
+    </>
+  );
+}
+
+export default FavoriteColor; // ✅ THIS LINE is required
